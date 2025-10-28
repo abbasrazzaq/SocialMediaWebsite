92ffb37c0b7e58c88367b025199e7d2241fd555c
abbas <abbas@ABBAS-THINKPAD>
2025-10-28 14:40:41 +0000

    initial commit

---
+++ SocialMediaWebsite.Server/Controllers/UserProfileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaWebsite.Server.Controllers
+{
+    public class UserProfileInfo
+    {
+        public required string Username { get; set; }
+        public required string Fullname { get; set; }
+        public int Age { get; set; }
+        public required string Location { get; set; }
+        public required string Workplace { get; set; }
+        public required string Hometown { get; set; }
+        public required string StudiedAt { get; set; }
+    };
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserProfileController : Controller
+    {
+        [HttpGet("getprofileinfo")]
+        public IActionResult GetProfileInfo()
+        {
+            return Ok( 
+                new UserProfileInfo
+                {
+                    Username = "abbashols",
+                    Fullname = "Abbas Razzaq",
+                    Age = 37,
+                    Location = "Ho Chi Minh City",
+                    Workplace = "Senior Software Developer at Surpass Assessment",
+                    Hometown = "Bradford",
+                    StudiedAt = "University of Derby"
+                }
+            );
+        }
+    }
+}
---
+++ socialmediawebsite.client/src/UserProfile.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from 'react';
+import type { CSSProperties } from 'react';
+
+interface UserProfile {
+    username: string;
+    fullname: string;
+    age: number;
+    location: string;
+    workplace: string;
+    hometown: string;
+    studiedAt: string;
+}
+
+function Profile() {
+    const [userProfile, setUserProfile] = useState<UserProfile>(null);
+
+    useEffect(() => {
+        async function loadUserProfile() {
+            const response = await fetch('api/userprofile/getprofileinfo',
+                {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" },
+                    body: null
+                });
+
+            if (response.ok) {
+                const data = await response.json();
+                setUserProfile(data);
+
+            }
+        }
+
+        loadUserProfile();
+
+    }, [userProfile]);
+
+    const contents = userProfile === null
+        ? <p>Loading...</p>
+        : <div>
+            <div style={profileDiv}>
+                <h1>User Profile</h1>
+                <label style={profileDivLabel}>
+                    <span style={profileDivSpan}>Username</span>
+                    <span style={profileDivSpan}>{userProfile.username}</span>
+                </label>
+            </div>
+
+            <div style={profileDiv}>
+                <h2>Bio</h2>
+
+                <label style={profileDivLabel}>
+                    <span style={profileDivSpan}>Name:</span>
+                    <span style={profileDivSpan}>{userProfile.fullname}</span>
+                </label>
+
+                <label style={profileDivLabel}>
+                    <span style={profileDivSpan}>Age:</span>
+                    <span style={profileDivSpan}>{userProfile.age}</span>
+                </label>
+
+                <label style={profileDivLabel}>
+                    <span style={profileDivSpan}>Lives:</span>
+                    <span style={profileDivSpan}>{userProfile.location}</span>
+                </label>
+
+                <label style={profileDivLabel}>
+                    <span style={profileDivSpan}>Workplace:</span>
+                    <span style={profileDivSpan}>{userProfile.workplace}</span>
+                </label>
+
+                <label style={profileDivLabel}>
+                    <span style={profileDivSpan}>From:</span>
+                    <span style={profileDivSpan}>{userProfile.hometown}</span>
+                </label>
+
+                <label style={profileDivLabel}>
+                    <span style={profileDivSpan}>Studied at:</span>
+                    <span style={profileDivSpan}>{userProfile.studiedAt}</span>
+                </label>
+            </div>
+            </div>;
+
+    return (
+
+        <div>
+            {contents}
+        </div>
+        
+            
+    );
+}
+
+export default Profile;
+
+
+const profileDiv: CSSProperties = {
+    color: 'black'
+};
+
+const profileDivLabel: CSSProperties = {
+    display: 'flex'
+};
+
+const profileDivSpan: CSSProperties = {
+    display: 'flex',
+    maxWidth: '250px',
+    padding: '24px',
+};
